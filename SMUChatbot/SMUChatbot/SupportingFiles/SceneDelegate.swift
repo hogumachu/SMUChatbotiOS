37229c54ac3fdfc8186df7596165878af7a7ba90
hogumachu <castle_jun@daum.net>
2021-12-27 11:30:39 +0900

    Removed space, Renamed

--- SMUChatbot/SMUChatbot/SupportingFiles/SceneDelegate.swift
+++ SMUChatbot/SMUChatbot/SupportingFiles/SceneDelegate.swift
@@ -1,18 +1,16 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = scene as? UIWindowScene else {
             return
         }
+        
         window = UIWindow(windowScene: scene)
         
         let appDependency = AppDependency.resolve(window: window!)
-        
         appDependency.start()
     }
 
